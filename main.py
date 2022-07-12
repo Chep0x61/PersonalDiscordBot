f133561549d843ca836d393558f09d31b7819bfa
Benjamin Reigner <benjamin.reigner@epitech.eu>
2022-07-12 17:18:43 +0200

    fix(help-command): add it in the ctor of Bot

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ from Help.help import EmbeddedHelp
 from Commands import Ping, Memes, CSGO
 from Events import BotEvents
 
-client = commands.Bot(command_prefix='!', help_command=None)
-
 
 def main():
     load_dotenv()
+
+    client = commands.Bot(command_prefix='!', help_command=EmbeddedHelp())
 
     client.add_cog(BotEvents.BotEvents(client))
     client.add_cog(Ping.Ping(client))
     client.add_cog(Memes.Memes(client))
     client.add_cog(CSGO.CSGO(client))
 
-    client.run(os.getenv('token'), help_command=EmbeddedHelp())
+    client.run(os.getenv('token'))
 
 
 if __name__ == "__main__":
